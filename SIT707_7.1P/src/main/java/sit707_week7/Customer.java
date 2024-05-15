af7d249d339d6b8de5f69c6fe1e92f51d3785878
Ishini Bhagya <59154447+ishini98@users.noreply.github.com>
2024-05-15 22:06:03 +0530

    Add files via upload

---
+++ SIT707_7.1P/src/main/java/sit707_week7/Customer.java
@@ -0,0 +1,12 @@
+package sit707_week7;
+
+public class Customer {
+	
+	private String firstName = "Ishini";
+	
+	private String lastName = "Bhagya";
+	
+	private String email = "Ishinibhagya@deakin.edu.au";
+	
+	
+}
